1fe3d586e15e2016eb4697963ab3dab2bbfb97ca
Levi1-a <78671585+Levi1-a@users.noreply.github.com>
2021-03-24 18:30:00 -0400

    3/24 change

---
+++ 3d platformer start/Assets/Animations/SceneSequence.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSequence : MonoBehaviour
+{
+
+    public GameObject Cam1;
+
+    void Start()
+    {
+        StartCoroutine (TheSequence());
+    }
+
+    IEnumerator TheSequence ()
+    {
+        yield return new WaitForSeconds(4);
+        Cam1.SetActive(false);
+        
+    }
+
+}
--- 3d platformer start/Assets/Scrips/PlayerMovement.cs
+++ 3d platformer start/Assets/Scrips/PlayerMovement.cs
@@ -2,13 +2,22 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    //rigidbody reference "rb"
     public Rigidbody rb;
+    public CharacterController controller;
+    public Transform cam;
 
-    public float speed = 20f;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     public float upForce = 2f;
     public bool canJump;
     public bool isGrounded;
+    public Animator anime;
+
+    private void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
 
 
     void OnCollisionStay()
@@ -20,26 +29,21 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 pos = transform.position;
 
-        if (Input.GetKey("w"))
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
         {
-            pos.z += speed * Time.deltaTime;
-        }
-        if (Input.GetKey("s"))
-        {
-            pos.z -= speed * Time.deltaTime;
-        }
-        if (Input.GetKey("d"))
-        {
-            pos.x += speed * Time.deltaTime;
-        }
-        if (Input.GetKey("a"))
-        {
-            pos.x -= speed * Time.deltaTime;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
-        transform.position = pos;
 
         //Jump
         if (canJump)
@@ -53,5 +57,15 @@ public class PlayerMovement : MonoBehaviour
             isGrounded = false;
         }
 
+        if (isGrounded == false)
+        {
+            anime.SetBool("isJumping", true);
+        }
+        else
+        {
+            anime.SetBool("isJumping", false);
+        }
     }
+
+
 }
